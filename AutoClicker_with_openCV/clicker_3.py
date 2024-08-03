6d37b6e589c42e28cd0c5f012110797866dae35f
artem <artemgudc@gmail.com>
2024-08-03 20:12:51 +0200

    clicker_3.py +

--- AutoClicker_with_openCV/clicker_3.py
+++ AutoClicker_with_openCV/clicker_3.py
@@ -1,25 +1,54 @@
 import pyautogui
+import time
 
-path1 = 'SCREENS/screen1.png'
-path2 = 'SCREENS/screen2.png'
+image_paths = [
+    'SCREENS/screen1.png',
+    'SCREENS/screen2.png',
+    'SCREENS/screen3.png',
+    'SCREENS/screen4.png',
+    'SCREENS/screen5.png',
+    'SCREENS/screen6.png'
+]
+def main(image_paths):
+    i = 0
+    # Применение функции ко всем изображениям
+    for index, path in enumerate(image_paths):
+        find_and_click_image(path, i)  # Обычный клик для остальных
+        i += 10
+        pyautogui.sleep(1)  # Задержка между кликами
+    click_Hamser(image_paths[5])
 
-try:
-    button = pyautogui.locateOnScreen(path1, confidence = 0.5)
-    if button is not None:
-        pyautogui.doubleClick(button)
-        print("Двойной клик выполнен.")
-    else:
-        print("Изображение не найдено на экране.")
-except pyautogui.PyAutoGUIException as e:
-    print("Ошибка: ", e)
+    print("Скрипт завершен.")
 
-pyautogui.sleep(1)
+def find_and_click_image(image_path, time, confidence=0.7):
+    try:
+        button = pyautogui.locateOnScreen(image_path, time, confidence=confidence, region=(1740, 760, 180, 320))
+        if button:
+            pyautogui.click(button, button='right')
+            print(f"Клик выполнен для {image_path}.")
+        else:
+            print(f"Изображение {image_path} не найдено на экране.")
+    except pyautogui.PyAutoGUIException as e:
+        print(f"Ошибка при обработке {image_path}: ", e)
 
-try:
-    button = pyautogui.locateOnScreen(path2, confidence = 0.8)
-    if button is not None:
-        # pyautogui.moveTo(button)
-        pyautogui.click(button)
-        print("Клик выполнен.")
-except pyautogui.PyAutoGUIException as e:
-    print("Ошибка: ", e)
+
+def click_Hamser(image_path, clicks=500, delay=0.25, confidence=0.75, region=(1740, 760, 180, 320)):
+    try:
+        button = pyautogui.locateOnScreen(image_path, confidence=confidence, region=region)
+        if button:
+            while True:
+                for _ in range(clicks):
+                    pyautogui.click(button, button='SECONDARY')
+                    pyautogui.sleep(delay)
+
+                current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                print("Current Time:", current_time)
+                pyautogui.sleep(10 * 60)
+        else:
+            print(f"Изображение {image_path} не найдено на экране.")
+    except pyautogui.PyAutoGUIException as e:
+        print(f"Ошибка при обработке {image_path}: ", e)
+
+
+if __name__ == "__main__":
+    main(image_paths)
