e555292e49fad527e7b3b4de687430654e48f770
artem <artemgudc@gmail.com>
2024-09-18 23:56:54 +0300

    clicker_3.py

--- AutoClicker_with_openCV/cipher_Hamster.py
+++ AutoClicker_with_openCV/cipher_Hamster.py
@@ -16,32 +16,41 @@ img_Blue = r"SCREENS\ScreenBlue.png"
 def get_iform_cipher_Hamster():
     from datetime import datetime
     now = datetime.now()
-    day = now.day + 1
-    month = now.strftime("%B").lower()
-    link = f"https://sumorb.com/crypto/hamster-kombat-daily-cipher-and-combo-code-for-{day}-{month}-2024/"
-    print(link)
-    response = requests.get(link)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        if (ul_element := soup.find_all("ul", class_="wp-block-list")[1]):
-            cipher_codes = []
-            li_elements = ul_element.find_all("li")
-            for li in li_elements:
-                # Extract text within parentheses
-                text = li.get_text()
-                print(text)
-                start = text.find("(")
-                end = text.find(")")
-                if start != -1 and end != -1:
-                    cipher_code = text[start + 1:end]
-                    cipher_codes.append(cipher_code)
-            return cipher_codes
-        else:
-            print("Error: wp-block-list not find")
-            return None
-    else:
-        print("Failed to retrieve the webpage.")
-        return None
+    day = now.day
+    # month = now.strftime("%B").lower()
+    # link = f"https://sumorb.com/crypto/hamster-kombat-daily-cipher-and-combo-code-for-{day}-{month}-2024/"
+    # print(link)
+    # response = requests.get(link)
+    # if response.status_code == 200:
+    #     soup = BeautifulSoup(response.content, "html.parser")
+    #     if (ul_element := soup.find_all("ul", class_="wp-block-list")[1]):
+    #         cipher_codes = []
+    #         li_elements = ul_element.find_all("li")
+    #         for li in li_elements:
+    #             # Extract text within parentheses
+    #             text = li.get_text()
+    #             print(text)
+    #             start = text.find("(")
+    #             end = text.find(")")
+    #             if start != -1 and end != -1:
+    #                 cipher_code = text[start + 1:end]
+    #                 cipher_codes.append(cipher_code)
+    #         return cipher_codes
+
+    # link = f"https://www.cybersport.ru/tags/games/shifr-v-khomiake-na-{day}-sentiabria-hamster-kombat"
+    # print(link)
+    # response = requests.get(link)
+    # if response.status_code == 200:
+    #     soup = BeautifulSoup(response.content, "html.parser")
+    #     print(soup.prettify())  # Вывод содержимого страницы
+    #     # if (_element := soup.find_all())
+    #     # else:
+    #     #     print("Error: wp-block-list not find")
+    #     #     return None
+    # else:
+    #     print("Failed to retrieve the webpage.")
+    #     print(f"Ошибка: {response.status_code}")
+    #     return None
 
 
 def manual_input():
@@ -52,14 +61,21 @@ def manual_input():
     if cipher_code is not None:
         # Используем полученный код
         pyautogui.alert(f"Cipher code entered: {cipher_code}")
-        matches = re.findall(r'dash|dot|—|•|–|\.|_|-', cipher_code)
 
-        # Соединим их в строку с пробелами
-        result = ' '.join(matches)
+        # Разделяем ввод на строки
+        lines = cipher_code.split('\n')
 
-        print(result)
+        result = []
 
-        result = ["dash" if char in ['_', '—', '–', '-', 'dash'] else "dot" for char in matches]
+        for line in lines:
+            # Извлекаем только символы и игнорируем буквы
+            matches = re.findall(r'—|•|–|\.|_|-|tap|hold|▬|●', line)
+
+            # Преобразуем найденные символы в 'dash' и 'dot'
+            transformed = ["dash" if char in ['_', '—', '–', '-', 'dash', 'hold', '▬'] else "dot" for char in matches]
+
+            # Преобразуем список в строку с элементами через запятую
+            result.append(', '.join(transformed))
 
         print(result)
         return result
@@ -68,9 +84,9 @@ def manual_input():
         return None
 
 
-def open_icon_cipher_Hamster(region=region, confidence=0.9, interval=1):
+def open_icon_cipher_Hamster(region=region_0, confidence=0.9, interval=1):
     try:
-        if (button := pyautogui.locateOnScreen(img_Cipher, confidence=confidence, region=region)):
+        if (button := pyautogui.locateOnScreen(img_Cipher, confidence=confidence, region=region_0)):
             pyautogui.rightClick(button)
             time.sleep(interval)
             return True
@@ -79,37 +95,39 @@ def open_icon_cipher_Hamster(region=region, confidence=0.9, interval=1):
         print(f"Ошибка при обработке {img_Cipher}: {e}")
 
 
-def clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue, region=region, confidence=0.95, interval=0.5):
+def clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue, region=region_0, confidence=0.95, interval=0.5):
     try:
-        if (button := pyautogui.locateOnScreen(img_Cipher_Red, confidence=confidence, region=region)):
+        if (button := pyautogui.locateOnScreen(img_Cipher_Red, confidence=confidence, region=region_0)):
             # button_center = pyautogui.center(button)
+            # cipher_code = ['dot, dash, dash, dot', 'dot, dash, dot, dot', 'dot, dash', 'dash, dot, dash, dash']
             for code in cipher_code:
+                print(code)
                 symbols = code.split(", ")
                 for symbol in symbols:
                     print(symbol)
                     match symbol:
                         case "dot":
-                            time.sleep(interval // 2)  # Перерывать 0.25 секунду
+                            time.sleep(interval)  # Перерывать 0.25 секунду
                             pyautogui.rightClick(button)
-                            time.sleep(interval // 2)  # Перерывать 0.25 секунду
+                            time.sleep(interval / 2)  # Перерывать 0.25 секунду
                         case "dash":
-                            time.sleep(interval // 2)  # Перерывать 0.25 секунду
+                            time.sleep(interval / 2)  # Перерывать 0.25 секунду
                             # Придержать нажатие
                             pyautogui.mouseDown(button)
                             time.sleep(interval * 2)  # Удерживать нажатие 1 секунду
                             pyautogui.mouseUp()
-                            time.sleep(interval // 2)  # Перерывать 0.25 секунду
-                    # time.sleep(interval)
-                time.sleep(interval * 2)
+                            time.sleep(interval / 2)  # Перерывать 0.25 секунду
+                    # time.sleep(interval * 1)
+                time.sleep(interval * 8)
             return True
     except pyautogui.PyAutoGUIException as e:
         print(f"Ошибка при кликах по коду {cipher_code}: {e}")
 
 
-def get_coin(img_Cipher_Coin, region=region, confidence=0.75):
+def get_coin(img_Cipher_Coin, region=region_0, confidence=0.75):
     try:
-        time.sleep(10)
-        if (button := pyautogui.locateOnScreen(img_Cipher_Coin,region=region, confidence=confidence)):
+        time.sleep(5)
+        if (button := pyautogui.locateOnScreen(img_Cipher_Coin,region=region_0, confidence=confidence)):
             pyautogui.click(button)
             print("+1 000 000")
             return True
@@ -120,13 +138,13 @@ def get_coin(img_Cipher_Coin, region=region, confidence=0.75):
 def main_cipher_Hamster():
     if (cipher_code := get_iform_cipher_Hamster() or manual_input()):
         print(cipher_code)
-        if open_icon_cipher_Hamster():
-            if (clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue) or open_icon_cipher_Hamster() and clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue)):
-                if (get_coin(img_Cipher_Coin)):
-                    print('\033[32m Success with Hamster cipher \033[0m')
+        # if open_icon_cipher_Hamster():
+        if (clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue) or open_icon_cipher_Hamster() and clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue)):
+            if (get_coin(img_Cipher_Coin)):
+                print('\033[32m Success with Hamster cipher \033[0m')
 
-                    open_icon_cipher_Hamster()
-                    return True
+                open_icon_cipher_Hamster()
+                return True
             # else:
             #     if open_icon_cipher_Hamster():
             #         if (clicks_chipher_Hamster(cipher_code, img_Cipher_Red, img_Blue)):
