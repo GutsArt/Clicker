236b0327cb535de817d7400a3fbf57f77a038edb
artem <artemgudc@gmail.com>
2024-09-27 23:55:53 +0200

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from pynput.keyboard import Listener, Key, KeyCode
 
+click_all, click_max = 0, 500
 
-toggle_key = KeyCode(char='p')
+toggle_key = KeyCode(char='+')
 exit_key = Key.esc
 
 clicking = False
